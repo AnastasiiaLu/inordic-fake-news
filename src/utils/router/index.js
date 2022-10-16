5a666c20c458208e45ed313bc4b40ded5bdae316
AnastasiiaLu <lu.anastasia@gmail.com>
2022-10-16 12:33:50 +0300

    Version 1
    1-я половина занятия

---
+++ src/utils/router/index.js
@@ -0,0 +1,31 @@
+import {createWebHistory, createRouter} from "vue-router";
+
+//Импортируем компоненты, которые будут отображаться на разных страницах
+import HelloWorld from "../../components/HelloWorld.vue";
+import NewsList from "../../components/NewsList.vue";
+
+//Делаю конфигурацию, который показывает опр. компонент при переходе по опр. пути
+//При переходе на / отображаем компонент HelloWorld
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: HelloWorld
+    },
+    {
+        path: "/news",
+        name: "News",
+        component: NewsList
+    }
+]
+
+//
+const router = createRouter({
+    //Пока пропускаем разбор history
+    history: createWebHistory(),
+    //Передаём в поле при создании объекта роутер наш routes
+    routes: routes,
+})
+
+//export default - это экспорт объекта по умолчанию, подходит для одного элемента
+export default router
